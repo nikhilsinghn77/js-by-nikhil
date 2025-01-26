e57ef10dbc18b14461612713193df7f79b20e591
Nikhil Singh Rajawat <nikhilsinghn77@gmail.com>
2025-01-26 11:29:25 +0000

    variables

---
+++ 01basic/02variables.js
@@ -0,0 +1,8 @@
+const accountID = 12345
+let accountemail ="nikhilsinghn777@gmail.com"
+var accountpassword ="1234556"
+accountcity ="Rampur"
+
+console.log(accountID);
+
+console.table([accountID,accountemail,accountpassword,accountcity])
