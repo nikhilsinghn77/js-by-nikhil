2392c54628a0cfa91abc18732c172f8b668a03d7
Nikhil Singh Rajawat <nikhilsinghn77@gmail.com>
2025-01-26 19:20:27 +0000

    conversionoperator

---
+++ 01basic/04conversionoperator.js
@@ -0,0 +1,11 @@
+// let score = true
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueNumber =Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+let inlogedin = 0
+let booleanISlogedin= Boolean(inlogedin)
+console.log(booleanISlogedin)
